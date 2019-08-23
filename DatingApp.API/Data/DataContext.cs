34902c1ef1cf7b39aa3e56037c4b65716482abf1
Viorel Dragos <viodragos@gmail.com>
2019-08-23 15:09:27 +0300

    Initial commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Models.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values {get; set;} 
+        
+    }
+}
